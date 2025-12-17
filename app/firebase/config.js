3db1c239ef02a54cc0eda8ba95acbe8dd0881d08
marcar1972-cyber <marcar1972@gmail.com>
2025-12-17 09:54:51 -0300

    Update config.js

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,17 +1,19 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // <--- NUEVO: Importar Firestore
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNhuSBBhmmQSV0BHC8MOtLKgAUpvtfWfE", // <-- Sacado de tu foto
+  apiKey: "AIzaSyDNhuSBBhmmQSV0BHC8MOtLKgAUpvtfWfE",
   authDomain: "auxiliar-pro.firebaseapp.com",
   projectId: "auxiliar-pro",
   storageBucket: "auxiliar-pro.appspot.com",
-  messagingSenderId: "342196612566", 
-  appId: "1:342196612566:web:c7f34365c4e10502e60877" // <-- Sacado de tu foto (ID de la app)
+  messagingSenderId: "342196612566",
+  appId: "1:342196612566:web:c7f34365c4e10502e60877"
 };
 
-// Inicialización segura para Next.js
+// Inicializar Firebase
 let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(firebase_app);
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(firebase_app); // <--- NUEVO: Exportamos la base de datos
