d18dad0a154de23ed7815cc36be301f57b56a30d
marcar1972-cyber <marcar1972@gmail.com>
2025-12-21 23:50:55 -0300

    Create index.js

---
+++ app/quizData/index.js
@@ -0,0 +1,12 @@
+// app/quizData/index.js
+import { level1 } from './level1';
+import { level2 } from './level2';
+import { level3 } from './level3';
+import { level4 } from './level4';
+
+export const LEVELS = [
+  level1,
+  level2,
+  level3,
+  level4
+];
