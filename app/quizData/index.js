80db9beb035ac1a3ef2f720cb28a2d438a728f89
marcar1972-cyber <marcar1972@gmail.com>
2026-01-14 23:03:39 -0300

    Update index.js

--- app/quizData/index.js
+++ app/quizData/index.js
@@ -2,10 +2,16 @@ import { level1 } from './level1';
 import { level2 } from './level2';
 import { level3 } from './level3';
 import { level4 } from './level4';
+import { level5 } from './level5';
+import { level6 } from './level6';
+import { level7 } from './level7';
 
 export const LEVELS = [
   level1,
   level2,
   level3,
-  level4
+  level4,
+  level5,
+  level6,
+  level7
 ];
